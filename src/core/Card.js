f82b3b2d6817e190952ca017b9d1bcdfb240b2f4
mdyaseenahmed <yaseenmd.cs19@bmsce.ac.in>
2022-06-16 00:12:21 +0530

    Update Card UI

--- src/core/Card.js
+++ src/core/Card.js
@@ -7,27 +7,30 @@ const Card = ({
 }) => {
     return (
         <div className="card myCard">
-               <div className="card-header lead">{file.fileName}</div>
+               <div className="card-header lead myCardHeader">{file.fileName}</div>
                <div className="card-body">
                     <ImageHelper file={file} />
                     <hr />
                     <p className="lead font-weight-normal mt-2">
                         {file.fileDescription ? file.fileDescription : "A Sample File"}
                     </p>
-                    <div className="row text-justify">
-                         <div className="col-12">
+                    <div className="row">
+                         <div className="col-12 text-justify">
                             <b>File Hash:</b> {file.fileHash ? file.fileHash.substring(0,20) + "..."  : "File hash"}
                          </div>
                          <br />
-                         <div className="col-12">
+                         <div className="col-12  text-justify">
                             <b>File Category:</b> {file.category ? file.category : "Unknown"}
                          </div>
-                         <div className="col-12">
+                         <div className="col-12  text-justify">
                             <b>Uploaded By:</b> {file.uploader.substring(0,16) + "..."}
                          </div>
-                         <div className="col-12">
+                         <div className="col-12  text-justify">
                             <b>Uploaded At:</b> {moment.unix(file.uploadTime).format('hh:mm:ss A DD/MM/Y')}
                          </div>
+                    </div>
+                    <div className="mt-4">
+                         <a href={"https://ipfs.infura.io/ipfs/" + file.fileHash} target="_blank" rel="noopener noreferrer" className="btn btn-block btn-outline-info">Get File</a>
                     </div>
                </div>
           </div>
